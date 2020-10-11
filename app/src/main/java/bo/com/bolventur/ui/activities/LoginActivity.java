9431c95f5f91e39e0e1899281c46dc0f09c2517d
WeimarAlexanderTorresHerrera <alexandertorreshe@gmail.com>
2020-10-11 10:30:12 -0400

    Login> UI login v.1.

--- app/src/main/java/bo/com/bolventur/ui/activities/LoginActivity.java
+++ app/src/main/java/bo/com/bolventur/ui/activities/LoginActivity.java
@@ -1,14 +1,16 @@
-package bo.com.bolventur;
+package bo.com.bolventur.ui.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import bo.com.bolventur.R;
+
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
 }
