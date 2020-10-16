a5bd1d67625aae4b02cc862738b260a5a8155809
SergiusxD <sbellott@hotmail.com>
2020-10-16 16:56:49 -0400

    Creating the MainMenu branch again in the correct branch

---
+++ app/src/main/java/bo/com/bolventur/viewModel/MainMenuViewModel.java
@@ -0,0 +1,12 @@
+package bo.com.bolventur.viewModel;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+
+public class MainMenuViewModel extends AndroidViewModel {
+    public MainMenuViewModel(@NonNull Application application) {
+        super(application);
+    }
+}
